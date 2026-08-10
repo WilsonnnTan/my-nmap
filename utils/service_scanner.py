71261ee023a47117f64a218f9251be9af0a3c329
WilsonnnTan <wilsonangelietann@gmail.com>
2026-08-10 19:59:08 +0700

    feat: add probe parser

--- utils/service_scanner.py
+++ utils/service_scanner.py
@@ -11,7 +11,7 @@ import re
 class Probe:
     protocol: Literal["UDP", "TCP"]
     probe_name: str
-    probe_string: str
+    probe_string: bytearray    # converted to byte during parsing
     no_payload: Literal["no-payload"] | None = None
 
 
@@ -41,12 +41,13 @@ class ServiceScan:
         
         self.excluded_port: list[Port] = []
         self.probes: list[ServiceProbe] = []
-        self.probes_counter = -1     # counter to track self.probes index
-        self.parse_probes_database()
+        self.probes_tracker = -1     # counter to track self.probes index
+        # self.parse_probes_database()
         
     def parse_probes_database(self):
         with open(self.probes_database, "r") as f:
             for line in f:
+                line = line.strip()
                 # Ignore comments
                 if line.startswith("#"):
                     continue
@@ -55,7 +56,7 @@ class ServiceScan:
                 elif line.startswith("Probe"):
                     self.probe_parser(line)
                 # continue if no probes exist in self.probes
-                elif self.probes_counter < 0:
+                elif self.probes_tracker < 0:
                     continue
                 elif line.startswith("match"):
                     self.match_parser(line)
@@ -101,8 +102,58 @@ class ServiceScan:
         self.excluded_port = result
     
     def probe_parser(self, line:str):
-        pass
-    
+        # https://en.wikipedia.org/wiki/Escape_sequences_in_C
+        escape_map = {
+            '\\': b'\x5c',  # Backslash
+            '0':  b'\x00',  # Null
+            'a':  b'\x07',  # Alert / Bell
+            'b':  b'\x08',  # Backspace
+            'f':  b'\x0c',  # Formfeed
+            'n':  b'\x0a',  # Newline
+            'r':  b'\x0d',  # Carriage Return
+            't':  b'\x09',  # Horizontal Tab
+            'v':  b'\x0b',  # Vertical Tab
+        }
+
+        regex = re.compile(
+            r'^Probe\s+(TCP|UDP)\s+(\S+)\s+q(.)((?:(?!\3).)*)\3(?:\s+(no-payload))?\s*$'
+        )
+
+        match = regex.match(line)
+        protocol, probe_name, delimeter, raw_probe_string, no_payload_flag = match.groups()
+        
+        # Decoded raw probe string into byte
+        probe_string = bytearray()
+        i = 0
+        
+        while i < len(raw_probe_string):
+            char = raw_probe_string[i]
+            if char == '\\' and i + 1 < len(raw_probe_string):
+                next_char = raw_probe_string[i + 1]
+                # \xHH -> (H is any hex digit)
+                if next_char == 'x' and i + 3 < len(raw_probe_string):
+                    hex_digits = raw_probe_string[(i + 2):(i + 4)]
+                    probe_string.append(int(hex_digits, base=16))
+                    i += 4
+                elif next_char in escape_map:
+                    probe_string += escape_map[next_char]
+                    i += 2
+            else:
+                probe_string.append(ord(char))
+                i += 1
+        
+        result = ServiceProbe(
+                    probe=Probe(
+                        protocol=protocol,
+                        probe_name=probe_name,
+                        probe_string=probe_string,
+                        no_payload=(True if no_payload_flag else False)
+                    )
+                )
+        
+        self.probes.append(result)
+        self.probes_tracker += 1
+
     def match_parser(self, line:str):
         pass
     
@@ -127,7 +178,7 @@ class ServiceScan:
                 ports.append(Port(port_number=int(part), protocol="TCP"))
                 ports.append(Port(port_number=int(part), protocol="UDP"))
 
-        self.probes[self.probes_counter].ports = ports
+        self.probes[self.probes_tracker].ports = ports
     
     def sslports_parser(self, line:str):
         # Extract portlist after "sslports "
@@ -147,28 +198,28 @@ class ServiceScan:
                 ports.append(Port(port_number=int(part), protocol="TCP"))
                 ports.append(Port(port_number=int(part), protocol="UDP"))
 
-        self.probes[self.probes_counter].sslports = ports
+        self.probes[self.probes_tracker].sslports = ports
     
     def totalwaitms_parser(self, line:str):
         # Extract time after "totalwaitms "
         match = re.search(r'totalwaitms\s+(\d+)(?![,\-])', line)
-        self.probes[self.probes_counter].totalwaitms = int(match.group(1))
+        self.probes[self.probes_tracker].totalwaitms = int(match.group(1))
     
     def tcpwrappedms_parser(self, line:str):
         # Extract time after "tcpwrappedms "
         match = re.search(r'tcpwrappedms\s+(\d+)(?![,\-])', line)
-        self.probes[self.probes_counter].tcpwrappedms = int(match.group(1))
+        self.probes[self.probes_tracker].tcpwrappedms = int(match.group(1))
     
     def rarity_parser(self, line:str):
         # Extract rarity after "rarity "
         match = re.search(r'rarity\s+(\d+)(?![,\-])', line)
-        self.probes[self.probes_counter].rarity = int(match.group(1))
+        self.probes[self.probes_tracker].rarity = int(match.group(1))
     
     def fallback_parser(self, line:str):
         # Extract fallback probes after "fallback "
         match = re.search(r'fallback\s+([\w,\-]+)', line)
-        self.probes[self.probes_counter].fallback = match.group(1).split(",")
+        self.probes[self.probes_tracker].fallback = match.group(1).split(",")
 
 
 scanner=ServiceScan()
-print(scanner.exclude_parser("Exclude 53,T:9100,U:30000-30002"))
+print(scanner.probe_parser("Probe UDP Quake3_getstatus q|\xff\xff\xff\xffgetstatus|"))
